8804b42ce62ed66b27aebbd277828c0b4cc42139
wikibootup <wikibootup@gmail.com>
2017-01-13 11:13:25 +0900

    http request from appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { HttpTestComponent } from './http-test/http-test.component';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   template: `
-    <app-http-test></app-http-test>
+    <button (click)="req()">button</button>
+  {{getData}}
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
+  getData;
+
+  constructor(private http: Http) {
+  }
+
+  req() {
+    this.getData = this.http.get('http://data.jsontest.com').
+      map(res => res.json());
+
+    console.log(this.getData);
+  }
 }
